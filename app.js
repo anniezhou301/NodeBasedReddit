7016a3555d097b91e700fb63ed8f19260ed5a279
Annie <anniezhou301@gmail.com>
2016-01-16 01:16:27 -0500

    Upvote System Implemented

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ app.controller('MainCtrl',[
 	'$scope',
 	function($scope){
 		$scope.posts = [
-  'post 1',
-  'post 2',
-  'post 3',
-  'post 4',
-  'post 5'
+  {title: 'post 1', upvotes: 5},
+  {title: 'post 2', upvotes: 2},
+  {title: 'post 3', upvotes: 15},
+  {title: 'post 4', upvotes: 9},
+  {title: 'post 5', upvotes: 4}
 ];
-		$scope.test= 'Hello World';
+		$scope.test= 'Node Reddit';
+		$scope.addPost=function(){
+			if(!$scope.title || $scope.title === '') { alert("You need a title");
+			return; }
+			$scope.posts.push({
+				title:$scope.title, link: $scope.link, upvotes:0
+			});
+			$scope.title="";
+			$scope.link = '';
+		}
+		$scope.incrementPost=function(post){
+			post.upvotes++;
+		}
 	}
 	]);
 
