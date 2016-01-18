6f3772611e9e61fdb5fec3a40426d34a4d909f0a
Annie <anniezhou301@gmail.com>
2016-01-17 19:32:53 -0500

    UI-routing

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-var app = angular.module("flapperNews",[]);
+var app = angular.module('flapperNews', ['ui.router'])
+
+app.factory('posts', [function(){
+	var o = {
+		posts: [  {title: 'post 1', upvotes: 5},
+		{title: 'post 2', upvotes: 2},
+		{title: 'post 3', upvotes: 15},
+		{title: 'post 4', upvotes: 9},
+		{title: 'post 5', upvotes: 4}]
+	};
+	return o;
+}]);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
+
 app.controller('MainCtrl',[
 	'$scope',
-	function($scope){
-		$scope.posts = [
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 15},
-  {title: 'post 4', upvotes: 9},
-  {title: 'post 5', upvotes: 4}
-];
+	'posts',
+	function($scope,posts){
+		$scope.posts = posts.posts;
+
 		$scope.test= 'Node Reddit';
 		$scope.addPost=function(){
 			if(!$scope.title || $scope.title === '') { alert("You need a title");
