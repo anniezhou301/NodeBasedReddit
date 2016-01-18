ead84f8dd23c6792b779b5885ced083a1412a8ba
Annie <anniezhou301@gmail.com>
2016-01-17 20:03:43 -0500

    done with angular posts
    bugs include unable to increment comments :/

--- app.js
+++ app.js
@@ -12,21 +12,45 @@ app.factory('posts', [function(){
 }]);
 
 app.config([
-'$stateProvider',
-'$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: '/home.html',
-      controller: 'MainCtrl'
-    });
+		$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		});
+		$stateProvider
+		.state('posts',{
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller:"PostsCtrl"
+		});
 
-  $urlRouterProvider.otherwise('home');
-}]);
 
+		$urlRouterProvider.otherwise('home');
+	}]);
 
+app.controller("PostsCtrl",[
+	"$scope",
+	"$stateParams",
+	"posts",
+	function($scope, $stateParams, posts){
+		$scope.post = posts.posts[$stateParams.id];
+		$scope.addComment = function(){
+			if($scope.body === '') { return; }
+			$scope.post.comments.push({
+				body: $scope.body,
+				author: 'user',
+				upvotes: 0
+			});
+			$scope.body = '';
+		};
+	}
+
+	]);
 app.controller('MainCtrl',[
 	'$scope',
 	'posts',
@@ -38,7 +62,13 @@ app.controller('MainCtrl',[
 			if(!$scope.title || $scope.title === '') { alert("You need a title");
 			return; }
 			$scope.posts.push({
-				title:$scope.title, link: $scope.link, upvotes:0
+				title:$scope.title, 
+				link: $scope.link,
+				upvotes:0,
+				comments:[
+				{author: "Bob", body: "Cool", upvotes: 0}, 
+				{author: "Caty", body: "Grool", upvotes:0}]
+				
 			});
 			$scope.title="";
 			$scope.link = '';
